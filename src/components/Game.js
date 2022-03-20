ca48cfc4e25fe7b5a45c93d4924791cf7aea61b5
Luisana Perez <luisanahenmaryp@gmail.com>
2022-03-20 13:35:21 -0400

    create the Game component

---
+++ src/components/Game.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const Game = () => {
+    return (
+        <>
+            <p>coming soon the board game</p>
+            <Link to="/" >Home</Link>
+        </>
+    )
+}
+
+export default Game
