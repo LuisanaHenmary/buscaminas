09ff6a1307a2829aecfc12f7ea9c96a6edd35b82
Luisana Perez <luisanahenmaryp@gmail.com>
2022-03-20 12:53:09 -0400

    create and add Menu component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import AppLayout from "./components/AppLayout"
+import { Route, Routes, Navigate } from "react-router-dom"
+import Menu from "./components/Menu"
+
 const App = () => {
   return (
-    <AppLayout>
-      
-    </AppLayout>
+
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Menu />} />
+          <Route
+            path="*"
+            element={<Navigate
+              replace
+              to="/"
+            />}
+          />
+        </Route>
+      </Routes>
+
   )
 }
 
---
+++ src/components/Menu.js
@@ -0,0 +1,9 @@
+const Menu = () =>{
+    return(
+        <div>
+            menu comming soon
+        </div>
+    )
+}
+
+export default Menu
